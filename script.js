070dfd73b818b35a419d6f3a29f35953bac13c74
Richa Dhusiya <richadhusiya@gmail.com>
2026-07-21 16:01:56 +0530

    Add image modal for project  screenshots

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+/*  project image view */ 
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+function openImage(src){
+    modal.style.display = "flex";
+    modalImg.src = src;
+
+}
+
+document.querySelector(".close").onclick = function(){
+    modal.style.display =  "none";
+};
+
+modal.onclick =  function(e){
+    if(e.target  === modal){
+        modal.style.display = "none";
+    }
+};
+
+
+
+/* contact button dropdown */ 
+
 const btn = document.getElementById("contactBtn");
 const dropdown = document.getElementById("contactDropdown");
 btn.addEventListener("click", function(e){
@@ -8,4 +34,12 @@ btn.addEventListener("click", function(e){
 
 document.addEventListener("click", function () {
     dropdown.classList.remove("show");
+});
+
+/* close img by clicking Esc key */
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape"){
+        modal.style.display = "none";
+    }
 });
